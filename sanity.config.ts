98741c89be9785491f2c7a3d3408779a46421b5b
Eray Demircioğlu <c.eray@hotmail.com>
2026-02-10 14:04:01 +0300

    Real Estate

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,28 +1,13 @@
-'use client'
-
-/**
- * This configuration is used to for the Sanity Studio that’s mounted on the `\app\studio\[[...tool]]\page.tsx` route
- */
-
-import {visionTool} from '@sanity/vision'
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-
-// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import {apiVersion, dataset, projectId} from './sanity/env'
-import {schema} from './sanity/schemas'
-import {structure} from './sanity/structure'
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import { schema } from "./sanity/schema";
 
 export default defineConfig({
-  basePath: '/studio',
-  projectId,
-  dataset,
-  // Add and edit the content schema in the './sanity/schemaTypes' folder
+  name: "zillow-clone",
+  title: "Real Estate Platform",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? "",
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET ?? "",
+  basePath: "/studio",
+  plugins: [structureTool()],
   schema,
-  plugins: [
-    structureTool({structure}),
-    // Vision is for querying with GROQ from inside the Studio
-    // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({defaultApiVersion: apiVersion}),
-  ],
-})
+});
